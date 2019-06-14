7bba12d377a9c1f9c83a54689caef2452c700e37
hansvana <hansvanarken@gmail.com>
2019-06-14 11:30:29 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RFIDscanner
 Turns RFID's received by XBee (or any other serial device) into keystrokes
+
+## Requirements
+
+- Tested on Windows 10 with Python 3.6
+- [pyserial](https://pypi.org/project/pyserial/)
+
+## Usage
+
+The usage case for this script is to quickly scan a large number of RFID ids into an Excel spreadsheet.
+
+This script expects to receive a number string (hex codes 48 to 57) followed by a newline (hex 10). Upon receiving the newline, it will simulate a keypress of each number followed by a tab. Every RFID tag found is stored in memory, to avoid duplicates.
+
+- Run the script in the background
+- Open Excel and select the first cell
+- Scan the RFID tags
+
+- Press `ESC` to exit
+- Press `r` to clear RFID memory (to rescan a tag)
---
+++ rfidscanner.py
@@ -0,0 +1,65 @@
+from directkeys import PressKey
+import serial.tools.list_ports
+import msvcrt
+import time
+
+#
+# Set the COM port here, or 
+# set listcomports to True if you want to see a list of COM ports
+#
+comport = 'COM4'
+listcomports = False
+#
+#
+
+if listcomports:
+    comlist = serial.tools.list_ports.comports()
+    connected = []
+    for element in comlist:
+        connected.append(element.device)
+    print("Connected COM ports: " + str(connected))
+    exit()
+
+ser = serial.Serial(port=comport, baudrate=57600)
+
+print("Connected to: " + ser.portstr)
+print("Press ESC to exit")
+
+line = []
+rfids = []
+
+while True:
+
+    # check for keypresses
+    if msvcrt.kbhit():
+        # escape key
+        if ord(msvcrt.getch()) == 27:
+            break
+        # r key
+        if ord(msvcrt.getch()) == 114:
+            print("clearing rfid cache")
+            rfids = []
+    
+    # check for serial data
+    while ser.in_waiting:
+        for c in ser.read():
+            # if character is a number
+            if c >= 48 and c <= 57:
+                line.append(chr(c))
+            # if character is newline
+            if c == 10:
+                if not line in rfids:
+                    print("rfid: " + ''.join(line))
+
+                    # press every character key
+                    for char in line:
+                        PressKey(ord(char))
+
+                    # press tab key
+                    PressKey(0x09)
+
+                    rfids.append(line)
+                line = []
+                break
+
+ser.close()
